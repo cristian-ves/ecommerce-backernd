7b3c8ee94a030470cfac9f5be9965b11c6a237b9
alejandro <alejandrovasquezesc@gmail.com>
2025-10-28 15:58:12 -0600

    config

--- src/main/java/com/alejo/config/SecurityConfig.java
+++ src/main/java/com/alejo/config/SecurityConfig.java
@@ -32,7 +32,12 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://embryoid-appetizingly-sullivan.ngrok-free.dev",
+                "https://ecommerce-cv.netlify.app/"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
@@ -41,4 +46,5 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
+
 }
